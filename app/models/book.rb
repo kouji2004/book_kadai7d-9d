1451058dac894349c90e8fe63812926a66f469cf
EC2 Default User <ec2-user@ip-172-31-82-87.ec2.internal>
2022-06-04 07:59:13 +0000

    応用課題7d

---
+++ app/models/book.rb
@@ -0,0 +1,29 @@
+class Book < ApplicationRecord
+  belongs_to :user
+  has_many :book_comments, dependent: :destroy
+  has_many :favorites, dependent: :destroy
+
+  validates :title,presence:true
+  validates :body,presence:true,length:{maximum:200}
+  validates :rate, numericality: {
+    less_than_or_equal_to: 5,
+    greater_than_or_equal_to: 1
+  }, presence: true
+
+
+  def favorited_by?(user)
+    favorites.where(user_id: user.id).exists?
+  end
+
+  def self.search_for(content, method)
+    if method == 'perfect'
+      Book.where(title: content)
+    elsif method == 'forward'
+      Book.where('title LIKE ?', content+'%')
+    elsif method == 'backward'
+      Book.where('title LIKE ?', '%'+content)
+    else
+      Book.where('title LIKE ?', '%'+content+'%')
+    end
+  end
+end
